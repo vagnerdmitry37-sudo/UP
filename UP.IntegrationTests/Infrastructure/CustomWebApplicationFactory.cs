9298a85a8b2828a77804888a1a7408ca0f3ff64a
Vagner Dmitry <dmitry.vagner@tui.co.uk>
2026-08-13 12:19:04 +0200

    actions 8

--- UP.IntegrationTests/Infrastructure/CustomWebApplicationFactory.cs
+++ UP.IntegrationTests/Infrastructure/CustomWebApplicationFactory.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Security.Cryptography;
 using UP.Api.Db;
 
 namespace UP.IntegrationTests.Infrastructure
@@ -13,11 +14,13 @@ namespace UP.IntegrationTests.Infrastructure
 
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
-            builder.ConfigureAppConfiguration((options, config) =>
+            builder.ConfigureAppConfiguration((context, config) =>
             {
+                var jwtKey = Convert.ToBase64String(RandomNumberGenerator.GetBytes(64));
+
                 config.AddInMemoryCollection(new Dictionary<string, string?>
                 {
-                    ["Jwt:Key"] = "4A8E7D3C91F2B6A5E8C1D9F4A7B2C6E9F1A3D5B7C8E2F4A6G9H1J3K5L7M9N2P"
+                    ["Jwt:Key"] = jwtKey
                 });
             });
 
