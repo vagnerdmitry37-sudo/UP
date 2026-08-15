0acb36e25f273000cb5fed8b3c1f1cc96d720ac5
Vagner Dmitry <dmitry.vagner@tui.co.uk>
2026-08-15 17:38:18 +0200

    deploy 7

--- Infrastructure/Terraform/Environments/Dev/main.tf
+++ Infrastructure/Terraform/Environments/Dev/main.tf
@@ -14,6 +14,81 @@ terraform {
   }
 }
 
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  # thumbprint_list = [
+  #   "6938fd4d98bab03faadb97b34396831e3780aea1"
+  # ]
+}
+
+resource "aws_iam_role" "github_actions" {
+  name        = "up-api-dev-github-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+
+        Action = "sts:AssumeRoleWithWebIdentity"
+
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = [
+              "repo:vagnerdmitry37-sudo/UP:environment:Dev"
+            ]
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "github_actions_s3_upload" {
+  name        = "GitHubActionsS3UploadPolicy"
+  description = "Allows GitHub Actions to upload files to S3"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:ListBucket"
+        ]
+
+        Resource = [
+          "arn:aws:s3:::up-app-terraform-state",
+          "arn:aws:s3:::up-app-terraform-state/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_s3_upload" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = aws_iam_policy.github_actions_s3_upload.arn
+}
+
 # provider "aws" {
 #   region = var.aws_region
 # }
