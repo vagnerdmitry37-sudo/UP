d90b2116ad3c5516eeeb5c1bc14ef9e5ffcc1273
Vagner Dmitry <dmitry.vagner@tui.co.uk>
2026-08-15 18:12:20 +0200

    deploy 11

--- Infrastructure/Terraform/Bootstrap/main.tf
+++ Infrastructure/Terraform/Bootstrap/main.tf
@@ -5,9 +5,9 @@ resource "aws_iam_openid_connect_provider" "github" {
     "sts.amazonaws.com"
   ]
 
-  # thumbprint_list = [
-  #   "6938fd4d98bab03faadb97b34396831e3780aea1"
-  # ]
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1"
+  ]
 }
 
 resource "aws_iam_role" "github_actions" {
